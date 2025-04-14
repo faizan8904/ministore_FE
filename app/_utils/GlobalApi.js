6cad6df2e4dba07827ec3aa13b4ad2b0f8f1ea3a
Faizan <faizansyed8904@gmail.com>
2025-04-14 15:03:58 +0530

    Update GlobalApi.js

--- app/_utils/GlobalApi.js
+++ app/_utils/GlobalApi.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios")
 
 const apikey = process.env.NEXT_PUBLIC_REST_API_KEY
-const apiUrl = 'http://localhost:1337/api'
+const apiUrl = process.env.NEXT_API_URL
 
 
 const axiosClient = axios.create({
